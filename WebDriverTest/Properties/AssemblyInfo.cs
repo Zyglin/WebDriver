6245ec2f2b7d6370af665aa535b355d1f84e1a6c
Zyglin <zyglin@mail.ru>
2018-11-13 00:15:05 +0300

    Add WebDriver

---
+++ WebDriverTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("WebDriverTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WebDriverTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3675dab5-eda4-464a-b9f7-2646385a5bbd")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ WebDriverTest/WebDriver.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Support.UI;
+
+namespace WebDriverTest
+{
+    [TestFixture]
+    public class WebDriver
+
+    {
+
+        [Test]
+        public void Test_write_flight()
+        {
+            RemoteWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.turkishairlines.com/");
+            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(15));
+            var btn = driver.FindElement(By.XPath("//a[@href='#bookerDeparTab']"));
+            btn.Click();
+            wait.Until(ExpectedConditions.ElementIsVisible(By.Id("membershipNumber"))).SendKeys("284");
+            var findFlight = driver.FindElement(By.XPath("//a[@data-bind='click : findFlights']"));
+            findFlight.Click();
+            var statusFlight = driver.FindElement(By.XPath("//div[@class='h4 img-align-right']"));
+            
+            if (statusFlight != null)
+            {
+
+                Assert.AreEqual("Минск (MSQ)", statusFlight.Text);
+            }
+         
+        }
+    }
+}
